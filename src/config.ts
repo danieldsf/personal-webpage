b16901954e227ee3f683f897bdf53a90a157ed12
Daniel Farias <daniel.dsfarias@gmail.com>
2022-03-13 11:01:12 -0300

    feat: add profile page and configs

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+import profile from './../public/profile.jpg'
+
+export const profilePicture = profile
+
+export const userConfig = {
+    name: 'Daniel Farias',
+    github: 'danieldsf',
+    description: 'Backend developer'
+}
+
+export const socials = [
+    { label: 'Linkedin', url: 'https://www.linkedin.com/in/danieldsfarias/', icon: 'bi bi-linkedin' },
+    { label: 'Youtube', url: 'https://www.youtube.com/channel/UC4zQ9O33go50K5Nh-lwO5LA', icon: 'bi bi-youtube' },
+    { label: 'E-mail', url: 'mailto:daniel.dsfarias@gmail.com', icon: 'bi bi-envelope' },
+]
+
+export const links = [
+    { label: 'Resume', url: 'https://www.dropbox.com/s/3d3rrb2y5ccimx3/DanielFarias_resume.pdf?dl=0' },
+    { label: 'Skills', url: 'https://danieldsf.notion.site/Personal-roadmap-Daniel-Farias-7b2572dc774246348aec7ec52c4091ba' },
+]
